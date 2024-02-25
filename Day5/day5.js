c02e800c2f3d03668b4691604ce418a7d507c288
lucasbarbosa0217 <lucasbarbosa0217@gmail.com>
2024-02-25 19:20:31 -0300

    Dia5 Final
    DIA 5 Final

--- Day5/day5.js
+++ Day5/day5.js
@@ -3,16 +3,48 @@ const laticinios = document.getElementById("input-laticinios").querySelectorAll(
 
 const add = document.getElementById("add")
 const inputArea = document.getElementById("input-area")
+const listList = document.getElementById("listList")
+
+
+const listas = JSON.parse(localStorage.getItem("minhaListaDeListas"))
+
+listas?.forEach((objeto, index) => {
+    listList.insertAdjacentHTML("beforeend", 
+    `
+        <div class="productList" onclick="mostrar(${index+1})">
+        <i class="fa-solid fa-x"></i><p>Lista número ${index+1}</p> <p>${objeto[0].produto}...</p>
+        </div>
+    `)
+})
+
+const formatador = new Intl.ListFormat('pt', {
+    style: "long",
+    type: "conjunction"
+})
+
+function mostrar(index){
+    var nomes = listas[index-1].map(objeto => objeto.produto);
+    console.log(nomes)
+    window.alert(formatador.format(nomes))
+}
+function remover(event){
+     if(event.target.parentNode.tagName ==="BUTTON"){
+        event.target.parentNode.parentNode.remove();
+     }else{
+        event.target.parentNode.remove();
+     }
+}
 add.addEventListener('click', () => {
     let html = `
     <div class="input-div" id="input-laticinios">
-        <button><i class="fa-solid fa-x"></i></button>
+        <button onClick="remover(event)"><i class="fa-solid fa-x" onClick="()"></i></button>
         <input type="text" class="text-input" placeholder="Nome de um produto">
     </div>
 `;
 
 inputArea.insertAdjacentHTML('afterbegin', html);
-
+const inputList = Array.from(inputArea.querySelectorAll('#input-laticinios input.text-input'));
+inputList[0].focus()
   
 })
 
@@ -20,10 +52,27 @@ const enviar = document.getElementById("enviar");
 
 enviar.addEventListener("click", () => {
     const inputList = Array.from(inputArea.querySelectorAll('#input-laticinios input.text-input'));
-    const inputValues = inputList.map(input => {
-        return input.value
-    })
+    const objetosProdutos = inputList.filter((valor, indice) => valor.value != "")
+    .map((valor, index) => {return {id: index+1, produto:valor.value}})
+    if(objetosProdutos.length === 0){return}
+    let lista = []
+    if(localStorage.getItem('minhaListaDeListas')){
+        lista = JSON.parse(localStorage.getItem('minhaListaDeListas'))
+    }else{
+        lista = []
+    };
+    lista.push(objetosProdutos)
+    var jsonList = JSON.stringify(lista)
+    console.log(lista)
+    if(lista){localStorage.setItem('minhaListaDeListas', jsonList);}
 
-    window.alert(inputValues)
 
+    listas.push(objetosProdutos)
+        listList.insertAdjacentHTML("beforeend", 
+        `
+            <div class="productList" onclick="mostrar(${listas.length+1})">
+            <i class="fa-solid fa-x"></i><p>Lista número ${listas.length+1}</p> <p>${objetosProdutos[0].produto}...</p>
+            </div>
+        `)
+   
 })
